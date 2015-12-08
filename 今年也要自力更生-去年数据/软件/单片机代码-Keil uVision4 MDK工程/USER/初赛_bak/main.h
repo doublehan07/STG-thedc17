fa784ff30dfa53664a693204cba0c4e007ca8700
haldak <haldakcis@gmail.com>
2015-12-08 21:47:24 +0800

    准备公开啦

--- 今年也要自力更生-去年数据/软件/单片机代码-Keil uVision4 MDK工程/USER/初赛_bak/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-uint8_t FindTan(uint16_t s);
-int16_t FixedTan(int16_t x, int16_t y);
-
-extern volatile uint8_t ChangeFlag;
-extern volatile uint8_t MakeNextMove;
-#endif
